5898a72d7161b030b5849169df6db1df998d527d
gYunTian <54625060+gYunTian@users.noreply.github.com>
2021-04-10 15:47:23 +0800

    Added page and function to search past games

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Button, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
@@ -9,7 +9,6 @@ import * as queries from './src/graphql/queries';
 
 // import Front from './src/navigation/Front'
 
-// const { w3cwebsocket: W3CWebSocket } = require('websocket')
 const { v4: uuidv4 } = require('uuid');
 const Stack = createStackNavigator();
 
@@ -22,35 +21,44 @@ const myAppConfig = {
 
 Amplify.configure(myAppConfig);
 
-const getData = async () => {
-  const todoDetails = {
-    "uid": "34"
-  };
-  const newTodo = await API.graphql({ query: queries.getGame, variables: {"uid": "34"}});
-  console.log("fetching data")
-  console.log(newTodo.data.getGame.date)
+const getData = async (uid: String) => {
+  const data = await API.graphql({ query: queries.getGame, variables: { "uid": uid } });
+  if (data.data.getGame === null) {
+    return 0
+  }
+  else {
+    console.log("fetched data")
+    return data.data.getGame
+  }
+}
+
+const createGame = async (uid: String, date: String, board: number[][]) => {
+  const createdGame = await API.graphql({
+    query: mutations.addGame, variables: {
+      "uid": uid,
+      "date": date,
+      "board": board
+    }
+  });
+  console.log("created game")
+  // console.log(createdGame)
 }
 
 const setUpNewSession = () => {
   console.log("creating new game session")
   let data = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-  let data2 = JSON.stringify(data)
-
-  let date = new Date()
+  let date = JSON.stringify(new Date())
   let uid = uuidv4()
 
   console.log("Creating new game")
-  // const newTodo = await API.graphql({ query: queries.getGame, variables: {"uid": "34"}});
-  console.log("fetching data")
-  // console.log(newTodo.data.getGame.date)
-
+  createGame(uid, date, data)
   console.log("New game created: " + uid)
-  // set up session
+
   return uid
 }
 
 const HomeScreen = ({ navigation }) => {
-  getData()
+  // getData()
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -69,6 +77,11 @@ const HomeScreen = ({ navigation }) => {
         title="Join existing Game"
         onPress={() => navigation.navigate('Game')}
       />
+      <Separator />
+      <Button
+        title="Find Game"
+        onPress={() => navigation.navigate('History')}
+      />
     </View>
   );
 }
@@ -81,9 +94,41 @@ const GameScreen = ({ route, navigation }) => {
 }
 
 const HistoryScreen = ({ route, navigation }) => {
-  const { uid } = route.params;
+  const [inputText, setInputText] = useState<String>("")
+  const [data, setData] = useState<any>(0)
+
+  const dataComp = async (inputText: any) => {
+    let details = await getData(inputText)
+    setData(details)
+  }
+
   return (
-    <Game uid={uid} navigation={navigation} />
+    <View style={styles.container}>
+
+      {data !== 0 ? (
+        <View>
+          <Text>Game ID: {data.uid}</Text>
+          <Text>Winner: {data.winner}</Text>
+          <Text>Final board: {data.board}</Text>
+          <Text>Moves in sequence: {data.moves1}</Text>
+          <Text>Player turns in sequence: {data.moves2}</Text>
+        </View>
+
+      ) : <Text>No data</Text>}
+
+      <TextInput
+        style={styles.input}
+        placeholder=" Search game id"
+        returnKeyLabel={"next"}
+        onChangeText={(text) => setInputText(text)}
+      />
+      <Button
+        title="Search game"
+        onPress={() => {
+          inputText.length > 0 ? (dataComp(inputText)) : console.log("empty")
+        }}
+      />
+    </View>
   );
 }
 
@@ -95,6 +140,7 @@ export default function App() {
 
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name="History" component={HistoryScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
@@ -114,6 +160,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
 
